65df3758f1a5d5f6346d07b3bedd708fa56c47a7
Micshoop2001 <Micshoop2001@yahoo.com>
2026-03-25 21:17:08 -0400

    commit

--- project/app.py
+++ project/app.py
@@ -1,14 +1,13 @@
 from application import create_app
 from application.models import db
-
-def create_app_instance():
-    app = create_app('DevelopmentConfig')
-    with app.app_context():
-        db.create_all()
-    return app
-'''
-app = create_app_instance()
+    
+app = create_app('DevelopmentConfig')    
+print(">>> app.py create_app reference:", create_app)
+    
+# Create the table
+with app.app_context():
+    #db.drop_all() #Drop all tables to reset the database
+    db.create_all()
 
 if __name__ == "__main__":
     app.run()
-'''
